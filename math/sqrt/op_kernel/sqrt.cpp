4f6cca060d47ffd7602deb65351141793e282324
liuxiqiang <liuxiqiang@noreply.gitcode.com>
2025-10-20 01:22:31 +0000

    sqrt 1020

--- math/sqrt/op_kernel/sqrt.cpp
+++ math/sqrt/op_kernel/sqrt.cpp
@@ -21,20 +21,20 @@ enum class SqrtTilingKey : uint32_t
     TILING_KEY_EXAMPLE_FLOAT = 0,
     TILING_KEY_EXAMPLE_INT32 = 1,
 };
-template <uint32_t schMode>
+// template <uint32_t schMode>
 __global__ __aicore__ void sqrt(GM_ADDR x, GM_ADDR y, GM_ADDR workspace, GM_ADDR tiling)
 {
     AscendC::printf("schMode: %d--------------------------------------------------------------------------------------------------------------------------------------------\n", schMode);
     REGISTER_TILING_DEFAULT(SqrtTilingData);
     GET_TILING_DATA_WITH_STRUCT(SqrtTilingData, tilingData, tiling);
-   if constexpr (schMode == static_cast<uint32_t>(SqrtTilingKey::TILING_KEY_EXAMPLE_INT32)){
-    MySqrt::KernelSqrt<DTYPE_X,DTYPE_Y> op; // 算子kernel实例获取
-    op.Init(x, y,tilingData->smallCoreDataNum,tilingData->bigCoreDataNum, tilingData->finalBigTileNum,tilingData->finalSmallTileNum, tilingData->tileDataNum,tilingData->smallTailDataNum, tilingData->bigTailDataNum,tilingData->tailBlockNum);      // 算子kernel实例初始化
-    op.Process();                       // 算子kernel实例执行
-   }
-   else {
+//    if constexpr (schMode == static_cast<uint32_t>(SqrtTilingKey::TILING_KEY_EXAMPLE_INT32)){
+//     MySqrt::KernelSqrt<DTYPE_X,DTYPE_Y> op; // 算子kernel实例获取
+//     op.Init(x, y,tilingData->smallCoreDataNum,tilingData->bigCoreDataNum, tilingData->finalBigTileNum,tilingData->finalSmallTileNum, tilingData->tileDataNum,tilingData->smallTailDataNum, tilingData->bigTailDataNum,tilingData->tailBlockNum);      // 算子kernel实例初始化
+//     op.Process();                       // 算子kernel实例执行
+//    }
+//    else {
     MySqrt::KernelSqrt<DTYPE_X,DTYPE_Y> op; // 算子kernel实例获取
     op.Init(x, y,tilingData->smallCoreDataNum,tilingData->bigCoreDataNum, tilingData->finalBigTileNum,tilingData->finalSmallTileNum, tilingData->tileDataNum,tilingData->smallTailDataNum, tilingData->bigTailDataNum,tilingData->tailBlockNum);      // 算子kernel实例初始化
     op.Process();                        // 算子kernel实例执行
-   }
+//    }
 }
