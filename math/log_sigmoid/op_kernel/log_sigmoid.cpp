593784e0eb5c5dc4a8bc123153e18d4f9d8de71c
qiu-zhuang <qiu-zhuang@noreply.gitcode.com>
2025-10-25 21:46:55 +0800

    LogSigmoid

---
+++ math/log_sigmoid/op_kernel/log_sigmoid.cpp
@@ -0,0 +1,40 @@
+/**
+ * This program is free software, you can redistribute it and/or modify.
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file log_sigmoid.cpp
+ * \brief
+ */
+
+#include "log_sigmoid.h"
+
+enum class LogSigmoidTilingKey : uint32_t
+{
+    TILING_KEY_FLOAT = 0,
+    TILING_KEY_FLOAT16 = 1,
+};
+
+template <uint32_t schMode>
+__global__ __aicore__ void log_sigmoid(GM_ADDR x, GM_ADDR z, GM_ADDR workspace, GM_ADDR tiling)
+{
+    REGISTER_TILING_DEFAULT(LogSigmoidTilingData);
+    GET_TILING_DATA_WITH_STRUCT(LogSigmoidTilingData, tilingData, tiling);
+    if constexpr (schMode == static_cast<uint32_t>(LogSigmoidTilingKey::TILING_KEY_FLOAT)) {
+        NsLogSigmoid::LogSigmoid<float> op; // 算子kernel实例获取
+        op.Init(x, z, tilingData);      // 算子kernel实例初始化
+        op.Process();                       // 算子kernel实例执行
+    }
+    if constexpr (schMode == static_cast<uint32_t>(LogSigmoidTilingKey::TILING_KEY_FLOAT16)) {
+        NsLogSigmoid::LogSigmoid<half> op; // 算子kernel实例获取
+        op.Init(x, z, tilingData);        // 算子kernel实例初始化
+        op.Process();                         // 算子kernel实例执行
+    }
+}
