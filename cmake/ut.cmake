e74b6e850bf887d3a2ec53acffc4b57011abead5
zhaozi3 <zhaozi3@h-partners.com>
2025-10-30 11:16:47 +0800

    adapt opbase pkg_inc

--- cmake/ut.cmake
+++ cmake/ut.cmake
@@ -30,7 +30,7 @@ if(UT_TEST_ALL OR OP_HOST_UT)
     target_include_directories(
       ${OP_TILING_MODULE_NAME}_common_obj PRIVATE ${JSON_INCLUDE_DIR} ${GTEST_INCLUDE}
                                                   ${ASCEND_DIR}/include/base/context_builder ${ASCEND_DIR}/pkg_inc
-                                                   ${ASCEND_DIR}/include/op_common ${ASCEND_DIR}/include/op_common/op_host
+                                                  ${OPBASE_INC_DIR}/op_common ${OPBASE_INC_DIR}/op_common/op_host
       )
     target_link_libraries(
       ${OP_TILING_MODULE_NAME}_common_obj PRIVATE $<BUILD_INTERFACE:intf_llt_pub_asan_cxx17> json gtest c_sec
@@ -43,8 +43,8 @@ if(UT_TEST_ALL OR OP_HOST_UT)
     target_include_directories(
       ${OP_TILING_MODULE_NAME}_cases_obj PRIVATE ${UT_COMMON_INC} ${GTEST_INCLUDE} ${ASCEND_DIR}/include
                                                  ${ASCEND_DIR}/include/base/context_builder ${PROJECT_SOURCE_DIR}/common/inc
-                                                 ${ASCEND_DIR}/include/op_common ${ASCEND_DIR}/include/tiling
-                                                 ${ASCEND_DIR}/include/op_common/op_host ${ASCEND_DIR}/include/toolchain
+                                                 ${OPBASE_INC_DIR}/op_common ${ASCEND_DIR}/include/tiling
+                                                 ${OPBASE_INC_DIR}/op_common/op_host ${ASCEND_DIR}/include/toolchain
       )
     target_link_libraries(${OP_TILING_MODULE_NAME}_cases_obj PRIVATE $<BUILD_INTERFACE:intf_llt_pub_asan_cxx17> gtest)
 
@@ -133,7 +133,7 @@ if(UT_TEST_ALL OR OP_KERNEL_UT)
     target_sources(${OP_KERNEL_MODULE_NAME}_common_obj PRIVATE ${OP_KERNEL_UT_COMMON_SRC})
     target_include_directories(
       ${OP_KERNEL_MODULE_NAME}_common_obj PRIVATE ${JSON_INCLUDE_DIR} ${GTEST_INCLUDE}
-                                                  ${ASCEND_DIR}/include/base/context_builder ${ASCEND_DIR}/pkg_inc
+                                                  ${ASCEND_DIR}/include/base/context_builder ${OPBASE_INC_DIR}
       )
     target_link_libraries(
       ${OP_KERNEL_MODULE_NAME}_common_obj PRIVATE $<BUILD_INTERFACE:intf_llt_pub_asan_cxx17> json gtest c_sec
@@ -275,9 +275,9 @@ if(UT_TEST_ALL OR OP_KERNEL_UT)
       add_library(${opName}_${socVersion}_tiling_tmp SHARED ${tilingSrc} $<TARGET_OBJECTS:${COMMON_NAME}_obj>)
       target_include_directories(
         ${opName}_${socVersion}_tiling_tmp
-        PRIVATE ${ASCEND_DIR}/include/op_common/atvoss ${ASCEND_DIR}/include/op_common
-                ${ASCEND_DIR}/include/op_common/op_host ${PROJECT_SOURCE_DIR}/common/inc
-                ${ASCEND_DIR}/include/tiling ${ASCEND_DIR}/include/op_common/op_host
+        PRIVATE ${OPBASE_INC_DIR}/op_common/atvoss ${OPBASE_INC_DIR}/op_common
+                ${OPBASE_INC_DIR}/op_common/op_host ${PROJECT_SOURCE_DIR}/common/inc
+                ${ASCEND_DIR}/include/tiling ${OPBASE_INC_DIR}/op_common/op_host
                 ${ASCEND_DIR}/include/toolchain
         )
       target_compile_definitions(${opName}_${socVersion}_tiling_tmp PRIVATE LOG_CPP _GLIBCXX_USE_CXX11_ABI=0)
@@ -333,8 +333,8 @@ if(UT_TEST_ALL OR OP_KERNEL_UT)
         ${opName}_${socVersion}_cases_obj
         PRIVATE ${ASCEND_DIR}/include/base/context_builder ${PROJECT_SOURCE_DIR}/tests/ut/op_kernel
                 ${PROJECT_SOURCE_DIR}/tests/ut/common ${PROJECT_SOURCE_DIR}/common/inc
-                ${ASCEND_DIR}/include/op_common ${ASCEND_DIR}/include/tiling
-                ${ASCEND_DIR}/include/op_common/op_host
+                ${OPBASE_INC_DIR}/op_common ${ASCEND_DIR}/include/tiling
+                ${OPBASE_INC_DIR}/op_common/op_host
         )
       target_link_libraries(
         ${opName}_${socVersion}_cases_obj PRIVATE $<BUILD_INTERFACE:intf_llt_pub_asan_cxx17> tikicpulib::${socVersion}
