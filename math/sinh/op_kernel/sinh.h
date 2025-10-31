c47a4491b833146a74b3314fed932709714f70d4
xiaopei-1 <xiaopei-1@noreply.gitcode.com>
2025-10-31 15:36:38 +0800

    迁移sinh算子

---
+++ math/sinh/op_kernel/sinh.h
@@ -0,0 +1,128 @@
+/**
+ * This program is free software, you can redistribute it and/or modify.
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file sinh.h
+ * \brief
+*/
+#ifndef SINH_H
+#define SINH_H
+
+#include "kernel_operator.h"
+#include "kernel_tiling/kernel_tiling.h"
+#include "sinh_tiling_data.h"
+#include "sinh_tiling_key.h"
+
+namespace NsSinh {
+
+using namespace AscendC;
+
+constexpr int32_t BUFFER_NUM = 2;
+
+template <typename T>
+class Sinh {
+public:
+    __aicore__ inline Sinh(){};
+
+    __aicore__ inline void Init(GM_ADDR x, GM_ADDR z, const SinhTilingData* tilingData);
+    __aicore__ inline void Process();
+
+private:
+    __aicore__ inline void CopyIn(int32_t progress);
+    __aicore__ inline void CopyOut(int32_t progress);
+    __aicore__ inline void Compute(int32_t progress);
+
+private:
+    TPipe pipe;
+    TQue<QuePosition::VECIN, BUFFER_NUM> inputQueueX;
+    TQue<QuePosition::VECOUT, BUFFER_NUM> outputQueueZ;
+    GlobalTensor<T> inputGMX;
+    GlobalTensor<T> outputGMZ;
+
+    uint32_t coreDataNum;
+    uint32_t tileNum;
+    uint32_t tileDataNum;
+    uint32_t tailDataNum;
+    uint32_t processDataNum;
+};
+
+template <typename T>
+__aicore__ inline void Sinh<T>::Init(GM_ADDR x, GM_ADDR z, const SinhTilingData* tilingData)
+{
+        ASSERT(AscendC::GetBlockNum() != 0 && "block dim can not be zero!");
+        uint32_t coreNum = AscendC::GetBlockIdx();
+        uint32_t globalBufferIndex = tilingData->bigCoreDataNum * AscendC::GetBlockIdx();
+        this->tileDataNum = tilingData->tileDataNum;
+        if (coreNum < tilingData->tailBlockNum) { 
+          this->coreDataNum = tilingData->bigCoreDataNum;
+          this->tileNum = tilingData->finalBigTileNum;
+          this->tailDataNum = tilingData->bigTailDataNum;
+        }
+        else { 
+          this->coreDataNum = tilingData->smallCoreDataNum;
+          this->tileNum = tilingData->finalSmallTileNum;
+          this->tailDataNum = tilingData->smallTailDataNum;
+          globalBufferIndex -= (tilingData->bigCoreDataNum - tilingData->smallCoreDataNum) * (AscendC::GetBlockIdx() - tilingData->tailBlockNum);
+        }
+        inputGMX.SetGlobalBuffer((__gm__ T*)x + globalBufferIndex, this->coreDataNum);
+        outputGMZ.SetGlobalBuffer((__gm__ T*)z + globalBufferIndex, this->coreDataNum);
+        pipe.InitBuffer(inputQueueX, BUFFER_NUM, this->tileDataNum * sizeof(T));
+        pipe.InitBuffer(outputQueueZ, BUFFER_NUM, this->tileDataNum * sizeof(T));
+
+    }
+
+template <typename T>
+__aicore__ inline void Sinh<T>::CopyIn(int32_t progress)
+{
+    AscendC::LocalTensor<T> xLocal = inputQueueX.AllocTensor<T>();
+    AscendC::DataCopy(xLocal, inputGMX[progress * this->tileDataNum], this->processDataNum);
+    inputQueueX.EnQue(xLocal);
+}
+
+template <typename T>
+__aicore__ inline void Sinh<T>::CopyOut(int32_t progress)
+{
+    AscendC::LocalTensor<T> zLocal = outputQueueZ.DeQue<T>();
+    AscendC::DataCopy(outputGMZ[progress * this->tileDataNum], zLocal, this->processDataNum);
+    outputQueueZ.FreeTensor(zLocal);
+}
+
+template <typename T>
+__aicore__ inline void Sinh<T>::Compute(int32_t progress)
+{
+    AscendC::LocalTensor<T> xLocal = inputQueueX.DeQue<T>();
+    AscendC::LocalTensor<T> zLocal = outputQueueZ.AllocTensor<T>();
+    T scalar = 0.5;
+    AscendC::Exp(xLocal, xLocal, this->processDataNum);
+    AscendC::Reciprocal(zLocal, xLocal, this->processDataNum);
+    AscendC::Sub(zLocal, xLocal, zLocal, this->processDataNum);
+    AscendC::Muls(zLocal, zLocal, scalar, this->processDataNum);
+    outputQueueZ.EnQue<T>(zLocal);
+    inputQueueX.FreeTensor(xLocal);
+}
+
+template <typename T>
+__aicore__ inline void Sinh<T>::Process()
+{
+    int32_t loopCount = this->tileNum;
+    this->processDataNum = this->tileDataNum;
+    for (int32_t i = 0; i < loopCount; i++) {
+        if (i == this->tileNum - 1) {
+            this->processDataNum = this->tailDataNum;
+        }
+        CopyIn(i);
+        Compute(i);
+        CopyOut(i);
+    }
+}
+
+} // namespace NsSinh
+#endif // Sinh_H
