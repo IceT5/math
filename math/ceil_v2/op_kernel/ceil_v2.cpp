efa2897d7c5921f502e808b341b32dbbf9beda5f
m0_62621784 <m0_62621784@noreply.gitcode.com>
2025-10-31 11:55:01 +0800

    迁移Ceil算子

---
+++ math/ceil_v2/op_kernel/ceil_v2.cpp
@@ -0,0 +1,40 @@
+/**
+ * This program is free software, you can redistribute it and/or modify it.
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file ceil_v2.cpp
+ * \brief
+*/
+
+#include "ceil_v2.h"
+
+enum class CeilV2TilingKey : uint32_t
+{
+    TILING_KEY_FLOAT = 0,
+    TILING_KEY_FLOAT16 = 1,
+};
+
+template <uint32_t schMode>
+__global__ __aicore__ void ceil_v2(GM_ADDR x, GM_ADDR z, GM_ADDR workspace, GM_ADDR tiling)
+{
+    REGISTER_TILING_DEFAULT(CeilV2TilingData);
+    GET_TILING_DATA_WITH_STRUCT(CeilV2TilingData, tilingData, tiling);
+    if constexpr (schMode == static_cast<uint32_t>(CeilV2TilingKey::TILING_KEY_FLOAT)) {
+        NsCeilV2::CeilV2<float> op; // 算子kernel实例获取
+        op.Init(x, z, tilingData);      // 算子kernel实例初始化
+        op.Process();                       // 算子kernel实例执行
+    }
+    if constexpr (schMode == static_cast<uint32_t>(CeilV2TilingKey::TILING_KEY_FLOAT16)) {
+        NsCeilV2::CeilV2<half> op; // 算子kernel实例获取
+        op.Init(x, z, tilingData);        // 算子kernel实例初始化
+        op.Process();                         // 算子kernel实例执行
+    }
+}
