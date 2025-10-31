e6c08a225983a3c0cbcf635d80f96503d9900dd1
TuYHAAAAAA <TuYHAAAAAA@noreply.gitcode.com>
2025-10-31 14:56:20 +0800

    迁移了mul，maximum和tan三个算子

---
+++ math/maximum_v2/op_kernel/maximum_v2.h
@@ -0,0 +1,158 @@
+/**
+ * This program is free software, you can redistribute it and/or modify.
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file maximum_v2.h
+ * \brief
+ */
+#ifndef MAXIMUMV2_H
+#define MAXIMUMV2_H
+
+#include "kernel_operator.h"
+#include "kernel_tiling/kernel_tiling.h"
+#include "maximum_v2_tiling_data.h"
+#include "maximum_v2_tiling_key.h"
+
+namespace NsMaximumV2 {
+
+using namespace AscendC;
+
+constexpr int32_t BUFFER_NUM = 2;
+
+template <typename T>
+class MaximumV2 {
+public:
+    __aicore__ inline MaximumV2(){};
+
+    __aicore__ inline void Init(GM_ADDR x, GM_ADDR y, GM_ADDR z, const MaximumV2TilingData* tilingData);
+    __aicore__ inline void Process();
+
+private:
+    __aicore__ inline void CopyIn(int32_t progress);
+    __aicore__ inline void CopyOut(int32_t progress);
+    __aicore__ inline void Compute(int32_t progress);
+
+private:
+    TPipe pipe;
+    TQue<QuePosition::VECIN, BUFFER_NUM> inputQueueX;
+    TQue<QuePosition::VECIN, BUFFER_NUM> inputQueueY;
+    TQue<QuePosition::VECOUT, BUFFER_NUM> outputQueueZ;
+    AscendC::TBuf<AscendC::TPosition::VECCALC> tmpBuf0;
+    AscendC::TBuf<AscendC::TPosition::VECCALC> tmpBuf1;
+    GlobalTensor<T> inputGMX;
+    GlobalTensor<T> inputGMY;
+    GlobalTensor<T> outputGMZ;
+
+    uint32_t coreDataNum;
+    uint32_t tileNum;
+    uint32_t tileDataNum;
+    uint32_t tailDataNum;
+    uint32_t processDataNum;
+};
+
+template <typename T>
+__aicore__ inline void MaximumV2<T>::Init(GM_ADDR x, GM_ADDR y, GM_ADDR z, const MaximumV2TilingData* tilingData)
+{
+        ASSERT(AscendC::GetBlockNum() != 0 && "block dim can not be zero!");
+        uint32_t coreNum = AscendC::GetBlockIdx();
+        uint32_t globalBufferIndex = tilingData->bigCoreDataNum * AscendC::GetBlockIdx();
+        this->tileDataNum = tilingData->tileDataNum;
+        if (coreNum < tilingData->tailBlockNum) { 
+          this->coreDataNum = tilingData->bigCoreDataNum;
+          this->tileNum = tilingData->finalBigTileNum;
+          this->tailDataNum = tilingData->bigTailDataNum;
+        }
+        else { 
+          this->coreDataNum = tilingData->smallCoreDataNum;
+          this->tileNum = tilingData->finalSmallTileNum;
+          this->tailDataNum = tilingData->smallTailDataNum;
+          globalBufferIndex -= (tilingData->bigCoreDataNum - tilingData->smallCoreDataNum) * (AscendC::GetBlockIdx() - tilingData->tailBlockNum);
+        }
+        inputGMX.SetGlobalBuffer((__gm__ T*)x + globalBufferIndex, this->coreDataNum);
+        inputGMY.SetGlobalBuffer((__gm__ T*)y + globalBufferIndex, this->coreDataNum);
+        outputGMZ.SetGlobalBuffer((__gm__ T*)z + globalBufferIndex, this->coreDataNum);
+        pipe.InitBuffer(inputQueueX, BUFFER_NUM, this->tileDataNum * sizeof(T));
+        pipe.InitBuffer(inputQueueY, BUFFER_NUM, this->tileDataNum * sizeof(T));
+        pipe.InitBuffer(outputQueueZ, BUFFER_NUM, this->tileDataNum * sizeof(T));
+        pipe.InitBuffer(tmpBuf0, this->tileDataNum * sizeof(T));
+        pipe.InitBuffer(tmpBuf1,  this->tileDataNum * sizeof(T));
+
+    }
+
+template <typename T>
+__aicore__ inline void MaximumV2<T>::CopyIn(int32_t progress)
+{
+    AscendC::LocalTensor<T> xLocal = inputQueueX.AllocTensor<T>();
+    AscendC::LocalTensor<T> yLocal = inputQueueY.AllocTensor<T>();
+    AscendC::DataCopy(xLocal, inputGMX[progress * this->tileDataNum], this->processDataNum);
+    AscendC::DataCopy(yLocal, inputGMY[progress * this->tileDataNum], this->processDataNum);
+    inputQueueX.EnQue(xLocal);
+    inputQueueY.EnQue(yLocal);
+}
+
+template <typename T>
+__aicore__ inline void MaximumV2<T>::CopyOut(int32_t progress)
+{
+    AscendC::LocalTensor<T> zLocal = outputQueueZ.DeQue<T>();
+    AscendC::DataCopy(outputGMZ[progress * this->tileDataNum], zLocal, this->processDataNum);
+    outputQueueZ.FreeTensor(zLocal);
+}
+
+template <typename T>
+__aicore__ inline void MaximumV2<T>::Compute(int32_t progress)
+{
+    AscendC::LocalTensor<T> xLocal = inputQueueX.DeQue<T>();
+    AscendC::LocalTensor<T> yLocal = inputQueueY.DeQue<T>();
+    AscendC::LocalTensor<T> zLocal = outputQueueZ.AllocTensor<T>();
+
+     if constexpr (std::is_same_v<T, bfloat16_t>) {
+        auto tmpX = tmpBuf0.Get<float>();
+        auto tmpY = tmpBuf1.Get<float>();
+        AscendC::Cast(tmpX, xLocal, AscendC::RoundMode::CAST_NONE, this->processDataNum);
+        AscendC::Cast(tmpY, yLocal, AscendC::RoundMode::CAST_NONE, this->processDataNum);
+        AscendC::Max(tmpX, tmpX, tmpY, this->processDataNum);
+        AscendC::Cast(zLocal, tmpX, AscendC::RoundMode::CAST_RINT, this->processDataNum);
+    }
+    else if constexpr (std::is_same_v<T, int8_t>) {
+        auto tmpX = tmpBuf0.Get<half>();
+        auto tmpY = tmpBuf1.Get<half>();
+        AscendC::Cast(tmpX, xLocal, AscendC::RoundMode::CAST_NONE, this->processDataNum);
+        AscendC::Cast(tmpY, yLocal, AscendC::RoundMode::CAST_NONE, this->processDataNum);
+        AscendC::Max(tmpX, tmpX, tmpY, this->processDataNum);
+        AscendC::Cast(zLocal, tmpX, AscendC::RoundMode::CAST_RINT, this->processDataNum);
+    }
+    else {
+        /* float/half/int16/int32 等直接 Max */
+        AscendC::Max(zLocal, xLocal, yLocal, this->processDataNum);
+    }
+    
+    outputQueueZ.EnQue<T>(zLocal);
+    inputQueueX.FreeTensor(xLocal);
+    inputQueueY.FreeTensor(yLocal);
+}
+
+template <typename T>
+__aicore__ inline void MaximumV2<T>::Process()
+{
+    int32_t loopCount = this->tileNum;
+    this->processDataNum = this->tileDataNum;
+    for (int32_t i = 0; i < loopCount; i++) {
+        if (i == this->tileNum - 1) {
+            this->processDataNum = this->tailDataNum;
+        }
+        CopyIn(i);
+        Compute(i);
+        CopyOut(i);
+    }
+}
+
+} // namespace NsMaximumV2
+#endif // MaximumV2_H
---
+++ math/tan_v2/op_kernel/tan_v2.cpp
@@ -0,0 +1,39 @@
+/**
+ * This program is free software, you can redistribute it and/or modify.
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file tan_v2.cpp
+ * \brief
+*/
+#include "tan_v2.h"
+
+enum class TanV2TilingKey : uint32_t
+{
+    TILING_KEY_EXAMPLE_FLOAT = 0,
+    TILING_KEY_EXAMPLE_HALF = 3,
+};
+
+template <uint32_t schMode>
+__global__ __aicore__ void tan_v2(GM_ADDR x, GM_ADDR z, GM_ADDR workspace, GM_ADDR tiling)
+{
+    REGISTER_TILING_DEFAULT(TanV2TilingData);
+    GET_TILING_DATA_WITH_STRUCT(TanV2TilingData, tilingData, tiling);
+    if constexpr (schMode == static_cast<uint32_t>(TanV2TilingKey::TILING_KEY_EXAMPLE_FLOAT)) {
+        NsTanV2::TanV2<float> op; // 算子kernel实例获取
+        op.Init(x, z, &tilingData);      // 算子kernel实例初始化
+        op.Process();                       // 算子kernel实例执行
+    }
+    else if constexpr (schMode == static_cast<uint32_t>(TanV2TilingKey::TILING_KEY_EXAMPLE_HALF)) {
+        NsTanV2::TanV2<half> op; // 算子kernel实例获取
+        op.Init(x, z, &tilingData);        // 算子kernel实例初始化
+        op.Process();                         // 算子kernel实例执行
+    }
+}
