d75ccfa0768f4a56da9eb908f78c98654387fef8
m0_62621784 <m0_62621784@noreply.gitcode.com>
2025-10-31 12:02:06 +0800

    迁移Trunc算子

---
+++ math/trunc_v2/op_graph/trunc_v2_graph_infer.cpp
@@ -0,0 +1,37 @@
+/**
+ * Copyright(c) Huawei Technologies Co., Ltd.2025. All rights reserved.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 2.0 (the "License");
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. See
+ * LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file trunc_v2_graph_infer.cpp
+ * \brief trunc_v2 operater graph infer resource
+ */
+#include "register/op_impl_registry.h"
+#include "log/log.h"
+
+namespace ops {
+using namespace ge;
+
+static constexpr int64_t IDX_0 = 0;
+
+static ge::graphStatus InferDataTypeTruncV2(gert::InferDataTypeContext* context)
+{
+    OP_LOGD(context->GetNodeName(), "Begin to do InferDataTypeTruncV2");
+
+    
+    ge::DataType sizeDtype = context->GetInputDataType(IDX_0);
+    context->SetOutputDataType(IDX_0, sizeDtype);
+
+    OP_LOGD(context->GetNodeName(), "End to do InferDataTypeTruncV2");
+    return GRAPH_SUCCESS;
+}
+
+IMPL_OP(TruncV2).InferDataType(InferDataTypeTruncV2);
+
+}; // namespace ops
