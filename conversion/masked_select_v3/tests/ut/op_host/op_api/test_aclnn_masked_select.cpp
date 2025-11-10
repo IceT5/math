28cc633d15712ccd3ffa2598916c9c5c56a23056
jisongyuan@h-partners.com <jisongyuan@h-partners.com>
2025-11-10 17:34:18 +0800

    opapi_ut

--- conversion/masked_select_v3/tests/ut/op_host/op_api/test_aclnn_masked_select.cpp
+++ conversion/masked_select_v3/tests/ut/op_host/op_api/test_aclnn_masked_select.cpp
@@ -12,7 +12,7 @@
 #include <vector>
 #include "gtest/gtest.h"
 
-#include "level2/aclnn_masked_select.h"
+#include "../../../../op_host/op_api/aclnn_masked_select.h"
 
 #include "op_api_ut_common/op_api_ut.h"
 #include "op_api_ut_common/scalar_desc.h"
